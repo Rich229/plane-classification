e2333ea62d6d4c429975e08501c2c9ec8ff38a34
Ulrich <aiounouu@gmail.com>
2022-04-10 20:04:35 +0200

    change path to locally update

--- app/app.py
+++ app/app.py
@@ -84,7 +84,7 @@ def predict_image(path, model,type_model,classes_names=NONE):
             "all_prob": _prediction_vector
         } 
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def load_model(path,type_model):
     """Load tf/Keras model for prediction
     """
